e71644615c0a8b5f299e40e3fb9ac7dbe182e538
keilsmit <keilsmit@yahoo.com>
2017-06-25 19:14:19 -0400

    can post and get Articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,8 +1,29 @@
-module.exports.create = function(){};
+
+
+var articles = [];
+
+module.exports.create = function(req, res){
+    console.log(req.body);
+    res.json(req.body);
+};
+
+/*
+a post is going to look like this:
+{
+  title: "",
+  body: "",
+  author: "",
+  comments: []
+}
+*/
+
 
 //rendering an html form to let user create post
 module.exports.new =  function(req, res){
-  res.send(<form method='post' action='/articles'>\
-            <input type='text' placeholder='name'\
-            </form>);
+  res.send("<form method='post' action='/articles'>\
+            <input type='text' placeholder='title' name='title'/>\
+            <input type='text' placeholder='author' name='author'/>\
+            <textarea placeholder='post' name'body'> </textarea>\
+            <button type='submit'>Post!</button>\
+            </form>");
 };
--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 var express = require('express');
-  app = express();
-  articles = require('./controllers/articles');
-/*
-    a post is going to look like this:
-    {
-      title: "",
-      body: "",
-      author: "",
-      comments: []
-    }
+var app = express();
+var articles = require('./controllers/articles');
+var bodyParser = require('body-parser');
 
+/*
     a comment would look like
     {
       name: "foo",
@@ -19,22 +13,26 @@ var express = require('express');
 
 //CRUD
 //Create Read Update Delete
+
+// app.use(express.logger());
+app.use(bodyParser.urlencoded({ extended: false }));
 var notImplemented = function(req, res){
     res.send(501)
-}
+};
 
 //articles
 app.get('/articles', notImplemented); // show them all blog posts
-app.get('articles/:articleId', notImplemented); //reading one
-app.get('articles/new', articles.new);
-app.post('/articles', notImplemented); //making a new posts
-app.put('article/:articleId', notImplemented); //updating one
-app.del('article/:articleId', notImplemented); //deleting one
+app.get('/articles/new', articles.new);
+app.get('/articles/:articleId', notImplemented); //reading one
+app.post('/articles', articles.create); //making a new posts
+app.put('/article/:articleId', notImplemented); //updating one
+app.del('/article/:articleId', notImplemented); //deleting one
 
 //Comments
 app.post('articles/:articleId/comments', notImplemented);
 app.del('articles/:articleId/comments/:commentId', notImplemented);
 
+//server on port 3000
 app. listen(3000, function(){
   console.log("Server is running...");
 });
